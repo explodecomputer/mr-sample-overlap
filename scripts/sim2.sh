b2a61169388faa207f651b3e08796ce2af9622ac
Gibran Hemani <explodecomputer@gmail.com>
2018-08-10 14:44:28 +0100

    updated

---
+++ scripts/sim2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --job-name=scenario2
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --array=1-144
+#SBATCH --output=job_reports/slurm-%A_%a.out
+#SBATCH --partition=mrcieu
+
+echo "Running on ${HOSTNAME}"
+module add R/3.2.3-foss-2016a
+
+if [ -n "${1}" ]; then
+  echo "${1}"
+  SLURM_ARRAY_TASK_ID=${1}
+fi
+
+i=${SLURM_ARRAY_TASK_ID}
+
+Rscript sim2.r 1000 ${i}
+
