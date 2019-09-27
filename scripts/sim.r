9412f1dac9a6b96991956411f83cd41cd6a0e6d5
ildar <i.sadreev@bristol.ac.uk>
2019-09-27 14:35:09 +0100

    Bugs fixed, parameter for replication added

--- scripts/sim.r
+++ scripts/sim.r
@@ -9,7 +9,7 @@ library(TwoSampleMR)
 #library(progress)
 
 
-# Function for running the algorithm on bc4
+# Function allowing many simulations at a time in bc4
 main <- function()
 {
   param <- expand.grid(
@@ -23,6 +23,9 @@ main <- function()
     gx = seq(0.04, 0.24, by=0.04),
     offset = seq(0, 10000, length.out=11)
   )
+  
+  # Adding the condition for replication: yes/no
+  param <- cbind(param,repl = "y")
   
   args <- commandArgs(T)
   splitsize <- as.numeric(args[1])
@@ -45,8 +48,8 @@ main <- function()
     out[[i]] <- suppressMessages(runsim(param[i,]))
   }
   out <- bind_rows(out)
-  dir.create("../results/sim5", recursive=TRUE, showWarnings=FALSE)
-  save(out, file=paste0("../results/sim5/out", chunk, ".rdata"))
+  dir.create("../results/sim", recursive=TRUE, showWarnings=FALSE)
+  save(out, file=paste0("../results/sim/out", chunk, ".rdata"))
 }
 
 # Function for running the simulations
@@ -83,79 +86,70 @@ runsim <- function(param)
   )
   res <- suppressMessages(mr(dat, method=c("mr_wald_ratio", "mr_ivw")))
   
-  # Why?
+  # Adding the results to param
   param$b <- res$b
   param$se <- res$se
   param$nsnp_inc <- nrow(dat)
   param$pval <- res$pval
-  # This causing Error: object of type 'builtin' is not subsettable
   param$mean_f <- mean(exp_d$fval, na.rm=TRUE)
-  
   param$obs <- lm(y ~ x)$coef[2]
   param$what <- "all"
   
-  # filter by most sifnificant SNPs (in mr or dat?)
-  dat_s <- subset(dat, pval.exposure < 5e-8)
+  # filter by most significant SNPs
+  dat <- subset(dat, pval.exposure < 5e-8)
   
   # SNPs from the exposure replication dataset that gave significant results
-  exp_r_s <- subset(exp_r, snp %in% dat_s$SNP)
+  exp_d_s <- subset(exp_d, snp %in% dat$SNP)
+  
+  # SNPs from the exposure replication dataset that gave significant results
+  exp_r_s <- subset(exp_r, snp %in% dat$SNP)
   
   # SNPs from the outcome dataset that correspond to the significant results
-  out_s <- subset(out, snp %in% dat_s$SNP)
+  out_s <- subset(out, snp %in% dat$SNP)
   
   # re-do the MR analysis for the significant SNPs, if there are any
-  if (nrow(dat_s) > 0)
+  if (nrow(dat) > 0)
   {
+    # specify the datasets for the MR analysis on the significant SNPs; the original dataset by default (no repl)
+    exp = exp_d_s
+    out = out_s
+    # check if the replication option selected
+    if (param$repl=="y")
+    {
+      exp = exp_r_s
+    }
+    
     dat <- data.frame(
-      SNP = 1:nrow(exp_r_s),
-      beta.exposure = exp_r_s$bhat,
-      beta.outcome = out_s$bhat,
-      se.exposure = exp_r_s$se,
-      se.outcome = out_s$se,
-      pval.exposure = exp_r_s$pval,
-      pval.outcome = out_s$pval,
-      fval.exposure = exp_r_s$fval,
+      SNP = 1:nrow(exp),
+      beta.exposure = exp$bhat,
+      beta.outcome = out$bhat,
+      se.exposure = exp$se,
+      se.outcome = out$se,
+      pval.exposure = exp$pval,
+      pval.outcome = out$pval,
+      fval.exposure = exp$fval,
       id.exposure=1,
       id.outcome=2,
       exposure=1,
       outcome=1,
       mr_keep=TRUE
     )
-    res <- suppressMessages(mr(dat, method=c("mr_wald_ratio", "mr_ivw")))
-  }
-  
-  
-  
-  if(nrow(dat) > 0)
-  {	
-    res2 <- suppressMessages(mr(dat, method="mr_wald_ratio", "mr_ivw"))
-    p2$b <- res$b
-    p2$se <- res$se
+    res2 <- suppressMessages(mr(dat, method=c("mr_wald_ratio", "mr_ivw")))
+    # Adding the results to p2
+    p2$b <- res2$b
+    p2$se <- res2$se
     p2$nsnp_inc <- nrow(dat)
-    p2$pval <- res$pval
+    p2$pval <- res2$pval
     p2$mean_f <- mean(dat$fval.exposure, na.rm=TRUE)
     p2$obs <- lm(y ~ x)$coef[2]
   }
   p2$what <- "sig"
+  
+  # Saving the output from both sig and all
   out <- bind_rows(param, p2)
   return(out)
+  
 }
 
-
-param <- expand.grid(
-  nid = 20000,
-  nsnp = 20,
-  maf = 0.3,
-  nsim = 1:10000,
-  xy = c(0, 0.2),
-  ux = 1,
-  uy = c(0, 0.6, 1, 2),
-  gx = seq(0.04, 0.24, by=0.04),
-  offset = seq(0, 10000, length.out=11)
-)
-
-runsim(param[100000,])
-
-dim(param)
 main()
 
